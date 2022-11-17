73e3a06b98b558a95f7a8d55e707612d8d5f3daf
Bryan <bryanbatista79688@gmail.com>
2022-11-17 12:59:32 -0300

    feat: Creates react files and starts Form component

---
+++ src/components/Form/index.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+const Form = ({  }) => {
+
+  return (
+
+    <form className="formContainer">
+      <input id="transactionValue" type="number"/>
+      <input id="transactionDesc" type="text"/>
+      <select id="transactionType" name="">
+        <option value="1">Entrada</option>
+        <option value="0">Saída</option>
+      </select>
+      <button id="insertValue" type="submit">Inserir valor</button>
+    </form>
+
+  );
+
+};
